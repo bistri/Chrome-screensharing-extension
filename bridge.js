d3e12f4d1451016cedb4fd9a1f153e887c026abb
Romain Caddoux <romain.caddoux@gmail.com>
2014-12-04 17:17:17 +0100

    extension detection system update

--- bridge.js
+++ bridge.js
@@ -1,5 +1,9 @@
 // open a port to communicate with background
 var port = chrome.runtime.connect();
+
+// create node
+var node = document.createElement( 'div' );
+
 // listen for messages from webpage and forward them to the background, through the previously opened port
 window.addEventListener( 'message', function ( event ){
     if ( event.source != window || !event.data ){
@@ -16,3 +20,7 @@ window.addEventListener( 'message', function ( event ){
 port.onMessage.addListener( function( data ){
     window.postMessage( data, '*' );
 } );
+
+// insert tag into parent page
+node.id = 'bistri-screen-sharing-installed';
+document.body.appendChild( node );
